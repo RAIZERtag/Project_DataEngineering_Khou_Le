7416101ba7d726cd8e14346ee905a6018e4f6d81
ChristopheLE2 <van-minhchristophe.le@edu.esiee.fr>
2024-02-08 08:31:55 +0100

    correctif scraping Temp et scrape img

--- data_collecting.py
+++ data_collecting.py
@@ -1,10 +1,13 @@
-import scrapy, json
+import scrapy
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import re
 import unidecode
+import os
+import requests
+from lxml import html
 
 
 class MySpider(scrapy.Spider):
@@ -20,8 +23,8 @@ class MySpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse, meta={'driver': self.driver})
 
     def parse(self, response):
-        def isCinqDernieresAnnees(equipe_year):
-            return int(re.search(r'\((\d{4})\)', equipe_year).group(1)) > 2018
+        def isVingtDernieresAnnees(equipe_year):
+            return int(re.search(r'\((\d{4})\)', equipe_year).group(1)) > 2003
         
         # Utilisez Selenium pour trouver l'élément image cliquable par XPath
         driver = response.meta['driver']
@@ -39,17 +42,42 @@ class MySpider(scrapy.Spider):
         # Une fois que la nouvelle page est chargée, récupérez le contenu
         table_data = driver.find_elements(By.XPATH, '//table[@class="TableGains"]/tbody/tr/td[2]/a')
         
-        nom_equipes = [element.text for element in table_data if element.text != "" and isCinqDernieresAnnees(element.text)]
+        nom_equipes = [element.text for element in table_data if element.text != "" and isVingtDernieresAnnees(element.text)]
 
         lien_details_equipes = [element.get_attribute("href") for element in table_data if element.text != ""]
         
         for equipe, lien in zip(nom_equipes, lien_details_equipes):
-            yield scrapy.Request(url=lien+"#Resume", callback=self.parse_subpage, meta={'equipe': equipe})
+            yield scrapy.Request(url=lien+"#Resume", callback=self.parse_subpage, meta={'equipe': equipe, 'url':'lien+"#Resume"'})
         
         # Fermez le navigateur Selenium
         driver.quit() 
     
     def parse_subpage(self, response):
+        def scrape_images(url, xpath_expression, save_folder, equipe_name):
+            # Créer le dossier s'il n'existe pas
+            if not os.path.exists(save_folder):
+                os.makedirs(save_folder)
+
+            # Récupérer le contenu de la page
+            # Utiliser lxml pour analyser la page HTML
+            tree = html.fromstring(response.text)
+            url_to_extract = tree.xpath(xpath_expression)
+            #return 1
+            # Utiliser XPath pour trouver les URLs des images
+            image_urls = url_to_extract[0].split("'")[1]
+
+            img_data =requests.get(image_urls).content
+
+            # Enregistrer l'image dans le dossier spécifié
+            with open(os.path.join(save_folder, f'{equipe_name}.jpg'), 'wb') as img_file:
+                img_file.write(img_data)
+
+        url_datasheet_group = response.meta['url']
+        xpath_expression_to_images = '//div[@class="imgEquipe"]/@style'
+        save_folder_path = "./images"
+        scrape_images(url_datasheet_group, xpath_expression_to_images, save_folder_path, response.meta['equipe'] )
+
+
         list_membres = response.xpath('//td[@style="text-align: center;"]/a/text()').extract()
         list_epreuves_keys = response.xpath(
 				'//h2[@class="titrePartie" and text()="Quête des clés"]'
@@ -81,7 +109,10 @@ class MySpider(scrapy.Spider):
         else:
             li_elements = response.xpath('//div[@class="blocSDTGain2"]/ul/li/text()')
             Gain = "".join(li.extract() if li.extract() != "O" else "0" for li in li_elements)
+
         Temp = response.xpath('//div[@class="sdt_temps"]/text()').extract()
+        for unit in range(len(Temp)):
+            Temp[unit] = int(Temp[unit])
 
         list_membres = [str(unidecode.unidecode(membre)) for membre in list_membres]
         list_epreuves_keys = [str(unidecode.unidecode(epreuve)) for epreuve in list_epreuves_keys]
